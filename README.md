181c87e50f22be9aa7e5fc43ac5f2079d1c5efda
Alex Gherman <alexgherman997@gmail.com>
2024-09-01 10:45:20 +0300

    add test cases

--- README.md
+++ README.md
@@ -4,15 +4,27 @@ Crawling data from web API with books information and validating it using bash s
 ## Installation steps  
 - clone repository 
 - chmod +x crawl-script.sh - give execution rights for script run
-- brew install jq - install jq command to parse JSON using 
+- brew install jq - install jq command to parse JSON for macOS 
 
 ## Execution 
 - ./crawl-script.sh
 
+# Validation explanation 
+App is doing 3 types of assertions:
+- performing assertions to check equality between each value from the web API and validation csv file 
+- performing assertions to check the type of the fields ID and Revision, and maximum length for fields Title and Subject
+- performing assertions to check if mandatory fields have value for Title, Subject and Revision
+
 ## Assertion structure
-Assertion for Book ID: OL1317211W 
-   Assertion passed: Title match!
-   Assertion passed: Subject match!
-   Assertion passed: Revision match!
-   Assertion passed: API Revision is numeric!
-   Assertion passed: Author in Description match!
+Assertion for Book ID: OL796465W
+- Assertion passed: Title match csv value!
+- Assertion passed: Subject match csv value!
+- Assertion passed: Revision match csv value!
+- Assertion passed: Author in Description match csv value!
+- Assertion passed: ID 'OL796465W' is valid.
+- Assertion passed: Title length is within limit.
+- Assertion passed: Subject length is within limit.
+- Assertion passed: Revision is numeric.
+- Assertion passed: Title is not empty.
+- Assertion passed: Subject is not empty.
+- Assertion passed: Revision is not empty.
--- crawl-script.sh
+++ crawl-script.sh
@@ -9,9 +9,9 @@ assert_equal() {
     local field_name="$3"
 
     if [[ "$csv_value" == "$api_value" ]]; then
-        echo "   Assertion passed: $field_name match!"
+        echo "   Assertion passed: $field_name match csv value!"
     else
-        echo "   Assertion failed: $field_name do not match!"
+        echo "   Assertion failed: $field_name do not match csv value!"
         echo "   CSV $field_name: $csv_value"
         echo "   API $field_name: $api_value"
     fi
@@ -24,11 +24,29 @@ assert_contains() {
     local field_name="$3"
 
     if [[ "$api_array" == *"$csv_value"* ]]; then
-        echo "   Assertion passed: $field_name match!"
+        echo "   Assertion passed: $field_name match csv value!"
     else
         echo "   Assertion failed: $field_name '$csv_value' not found in API $field_name!"
         echo "   CSV $field_name: $csv_value"
         echo "   API $field_name: $api_array"
+    fi
+}
+
+# Function to check if a key field exists and meets specific format criteria
+assert_id_format() {
+    local id="$1"
+
+    # Check if ID is not empty
+    if [[ -z "$id" ]]; then
+        echo "   Assertion failed: ID is missing."
+        return
+    fi
+
+    # Check if ID starts with 'OL' and ends with 'W'
+    if [[ "$id" =~ ^OL.*W$ ]]; then
+        echo "   Assertion passed: ID '$id' is valid."
+    else
+        echo "   Assertion failed: ID '$id' does not start with 'OL' or end with 'W'."
     fi
 }
 
@@ -37,12 +55,40 @@ assert_numeric() {
     local value="$1"
     local field_name="$2"
     if [[ "$value" =~ ^[0-9]+$ ]]; then
-        echo "   Assertion passed: $field_name is numeric!"
+        echo "   Assertion passed: $field_name is numeric."
     else
-        echo "   Assertion failed: $field_name is not numeric!"
+        echo "   Assertion failed: $field_name is not numeric."
         echo "   $field_name: $value"
     fi
 }
+
+# Function to check if a field exceeds a maximum length
+assert_max_length() {
+    local value="$1"
+    local max_length="$2"
+    local field_name="$3"
+
+    if [[ "${#value}" -le "$max_length" ]]; then
+        echo "   Assertion passed: $field_name length is within limit."
+    else
+        echo "   Assertion failed: $field_name length exceeds $max_length characters!"
+        echo "   $field_name: $value"
+    fi
+}
+
+# Function to check if a mandatory field is empty
+assert_not_empty() {
+    local value="$1"
+    local field_name="$2"
+
+    if [[ -n "$value" ]]; then
+        echo "   Assertion passed: $field_name is not empty."
+    else
+        echo "   Assertion failed: $field_name is empty!"
+        echo "   $field_name: $value"
+    fi
+}
+
 
 # Read the CSV file line by line
 while IFS=',' read -r id title subject revision author_in_description; do
@@ -68,17 +114,22 @@ while IFS=',' read -r id title subject revision author_in_description; do
     revision_book=$(echo "$response" | jq -r '.revision')
     author_in_description_book=$(echo "$response" | jq -r '.description')
 
-    # Perform assertions
-     echo "Assertion for Book ID: $id"
-
+    # Perform equality assertions between api and csv
+    echo "Assertion for Book ID: $id"
     assert_equal "$title_csv" "$title_book" "Title"
-    
     subject_match=$(echo "$response" | jq --arg subject "$subject_csv" '.subjects | index($subject)')
     assert_contains "$subject_csv" "$subject_book" "Subject"
-    
     assert_equal "$revision_csv" "$revision_book" "Revision"
-    assert_numeric "$revision_book" "API Revision"
-    
     assert_contains "$author_in_description_csv" "$author_in_description_book" "Author in Description"
 
+    # Performon field type assertions  
+    assert_id_format "$id"
+    assert_max_length "$title_book" 100 "Title"
+    assert_max_length "$subject_csv" 50 "Subject"
+    assert_numeric "$revision_book" "Revision"
+    
+    # Perform mandatory field assertions 
+    assert_not_empty "$title_book" "Title"
+    assert_not_empty "$subject_book" "Subject"
+    assert_not_empty "$revision_book" "Revision"
 done < "$csv_file"
